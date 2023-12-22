225bdf0ea86cd18538c4d68cacf0c56db8bbb25d
skdksldk <tnwns1485@naver.com>
2023-12-23 02:29:26 +0900

    Filters Modal

--- app/components/layout/Search.tsx
+++ app/components/layout/Search.tsx
@@ -1,30 +1,78 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import { BiSearch } from 'react-icons/bi';
+import { differenceInDays } from 'date-fns';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useCountries from '@/app/hooks/useCountries';
 
 const Search = () => {
-  return (
-    <div 
+  const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
+  return ( 
+    <div
+      onClick={searchModal.onOpen}
       className="
-        border-[1px]
-        w-full
-        md:w-auto
-        py-2
-        rounded-full
-        shadow-sm
-        hover:shadow-md
-        transition
+        border-[1px] 
+        w-full 
+        md:w-auto 
+        py-2 
+        rounded-full 
+        shadow-sm 
+        hover:shadow-md 
+        transition 
         cursor-pointer
       "
     >
-        <div 
-          className="
-            flex 
-            flex-row 
-            items-center 
-            justify-between
-          "
-        >
+      <div 
+        className="
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+        "
+      >
         <div 
           className="
             text-sm 
@@ -32,7 +80,7 @@ const Search = () => {
             px-6
           "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div 
           className="
@@ -46,7 +94,7 @@ const Search = () => {
             text-center
           "
         >
-          Any week
+          {durationLabel}
         </div>
         <div 
           className="
@@ -60,15 +108,21 @@ const Search = () => {
             gap-3
           "
         >
-          <div className="hidden sm:block">Add Guests</div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
+          <div className="hidden sm:block">{guestLabel}</div>
+          <div 
+            className="
+              p-2 
+              bg-rose-500 
+              rounded-full 
+              text-white
+            "
+          >
             <BiSearch size={18} />
           </div>
         </div>
       </div>
-
     </div>
   );
 }
-
+ 
 export default Search;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import RentModal from '@/app/components/modals/RentModal';
 import './globals.css'
 import getCurrentUser from './actions/getCurrentUser';
 import ClientOnly from './components/ClientOnly';
+import SearchModal from '@/app/components/modals/SearchModal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -30,6 +31,7 @@ export default async function RootLayout({
         <ClientOnly>
           <LoginModal />
           <RegisterModal />
+          <SearchModal />
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
